766ce5e92c268176bb281622804195245578aa6e
BrikerMan <eliyar917@gmail.com>
2019-05-18 12:19:48 +0800

    :sparkles: First draft of pre-processor.

--- kashgari/pre_processor.py
+++ kashgari/pre_processor.py
@@ -9,9 +9,13 @@
 # file: corpus.py
 # time: 2019-05-17 11:28
 
+import os
+import json
+import pickle
 import operator
 import collections
-from typing import List, Union
+import pathlib
+from typing import List, Union, Dict
 
 
 class PreProcessor(object):
@@ -28,7 +32,12 @@ class PreProcessor(object):
         self.label2idx = {}
         self.idx2label = {}
 
-    def build_token2idx_dict(self, tokenized_corpus: List[List[str]]):
+        self.token_pad = '<PAD>'
+        self.token_unk = '<UNK>'
+        self.token_bos = '<BOS>'
+        self.token_eos = '<EOS>'
+
+    def _build_token2idx_dict(self, tokenized_corpus: List[List[str]]):
         """
         Build token index dictionary using corpus
 
@@ -36,10 +45,10 @@ class PreProcessor(object):
             tokenized_corpus: List of tokenized sentences, like ``[['I', 'love', 'tf'], ...]``
         """
         token2idx = {
-            '<PAD>': 0,
-            '<UNK>': 1,
-            '<BOS>': 2,
-            '<EOS>': 3
+            self.token_pad: 0,
+            self.token_unk: 1,
+            self.token_bos: 2,
+            self.token_eos: 3
         }
 
         token2count = {}
@@ -60,9 +69,9 @@ class PreProcessor(object):
         self.token2idx = token2idx
         self.idx2token = dict([(value, key) for key, value in self.token2idx.items()])
 
-    def build_classification_label2idx_dict(self,
-                                            label_list: Union[List[str], List[List[str]]],
-                                            multi_label: bool = False):
+    def _build_classification_label2idx_dict(self,
+                                             label_list: Union[List[str], List[List[str]]],
+                                             multi_label: bool = False):
         """
         Build label2idx dict for classification labeling task
 
@@ -72,50 +81,40 @@ class PreProcessor(object):
         """
         pass
 
-    def build_seq_labeling_label2idx_dict(self,
-                                          label_list: List[List[str]]):
+    def _build_seq_labeling_label2idx_dict(self,
+                                           label_list: List[List[str]]):
         """
         Build label2idx dict for sequence labeling task
 
         Args:
             label_list: corpus label list
         """
-        pass
+        label2idx: Dict[str: int] = {}
 
+        token2count = {}
+        for sequence in label_list:
+            for label in sequence:
+                count = token2count.get(label, 0)
+                token2count[label] = count + 1
 
-    def numerize_token_sequence(self,
-                                sequence: List[str]) -> List[int]:
-        """
-        convert a token sequence to a numerical sequence, use the `self.token2idx` for mapping
-        Args:
-            sequence: a tokenized sequence
+        sorted_token2count = sorted(token2count.items(),
+                                    key=operator.itemgetter(1),
+                                    reverse=True)
+        token2count = collections.OrderedDict(sorted_token2count)
 
-        Returns:
-            Todo
-        """
-        pass
+        for token in token2count.keys():
+            if token not in label2idx:
+                label2idx[token] = len(label2idx)
 
-    def batch_numerize_token_sequence(self,
-                                      sequence_list: List[List[str]]) -> List[List[int]]:
-        """
-        batch function of `numerize_token_sequence`
-        Args:
-            sequence_list: list of tokenized sequences
+        self.label2idx = label2idx
+        self.idx2label = dict([(value, key) for key, value in self.label2idx.items()])
 
-        Returns:
-            Todo
-        """
-        return [self.numerize_token_sequence(seq) for seq in sequence_list]
-
-
-    def numerize_label_sequence(self,
-                                sequence: List[str]) -> List[int]:
-        pass
-
-
-    def preprocess_corpus(self, x, y, cache_dir):
+    def prepare_labeling_dicts_if_need(self,
+                                       x: List[List[str]],
+                                       y: List[List[str]]):
         """
         Process data and dump to cached path
+
         Args:
             x:
             y:
@@ -125,6 +124,87 @@ class PreProcessor(object):
 
         """
 
+        if not self.token2idx:
+            self._build_token2idx_dict(x)
+        if not self.label2idx:
+            self._build_seq_labeling_label2idx_dict(y)
+
+    def save_dicts(self, cache_dir: str):
+        pathlib.Path(cache_dir).mkdir(exist_ok=True, parents=True)
+        with open(os.path.join(cache_dir, 'token2idx.json'), 'w') as f:
+            f.write(json.dumps(self.token2idx, ensure_ascii=False, indent=2))
+
+        with open(os.path.join(cache_dir, 'label2idx.json'), 'w') as f:
+            f.write(json.dumps(self.label2idx, ensure_ascii=False, indent=2))
+
+    def load_cached_dicts(self, cache_dir: str):
+        with open(os.path.join(cache_dir, 'token2idx.json'), 'w') as f:
+            self.token2idx = json.loads(f.read())
+
+        with open(os.path.join(cache_dir, 'label2idx.json'), 'w') as f:
+            self.label2idx = json.loads(f.read())
+
+    def numerize_token_sequence(self,
+                                sequence: List[str]) -> List[int]:
+        """
+        convert a token sequence to a numerical sequence, use the `self.token2idx` for mapping
+
+        Args:
+            sequence: a tokenized sequence
+
+        Returns:
+            numeric represent of the token sequence
+        """
+        idx_seq = []
+        for token in sequence:
+            index = self.token2idx.get(token, self.token2idx[self.token_unk])
+            idx_seq.append(index)
+        return idx_seq
+
+    def numerize_label_sequence(self,
+                                sequence: List[str]) -> List[int]:
+        """
+        Convert label sequence to label-index sequence
+        ``['O', 'O', 'B-ORG'] -> [0, 0, 2]``
+
+        Args:
+            sequence: label sequence, list of str
+
+        Returns:
+            label-index sequence, list of int
+        """
+        return [self.label2idx[label] for label in sequence]
+
+    def reverse_numerize_label_sequence(self,
+                                        sequence: List[int],
+                                        length: int = 0) -> List[str]:
+        """
+        Convert label-index sequence to label sequence
+        ``[0, 0, 2] -> ['O', 'O', 'B-ORG']``
+
+        Args:
+            sequence: label-index sequence
+            length: max length of label sequence
+
+        Returns:
+            label sequence, list of str
+        """
+        token_seq = [self.idx2label[num] for num in sequence]
+        if length != 0:
+            token_seq = token_seq[:length]
+        return token_seq
+
 
 if __name__ == "__main__":
-    print("Hello world")
+    from kashgari.corpus import ChineseDailyNerCorpus
+
+    x, y = ChineseDailyNerCorpus.load_data()
+    p = PreProcessor()
+    p.prepare_labeling_dicts_if_need(x, y)
+    p.save_dicts('./cache-dir')
+    # p.build_token2idx_dict(x)
+    # p.build_seq_labeling_label2idx_dict(y)
+    # print(p.token2idx)
+    # print(p.label2idx)
+    # print(p.batch_numerize_token_sequence(x[:10]))
+    # print("Hello world")
