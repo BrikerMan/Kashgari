e670ba5f732d39df131d70726204e833ec1ab072
BrikerMan <eliyar917@gmail.com>
2019-05-18 13:01:51 +0800

    :sparkles: add SMP2018 ECDT dataset.

--- docs/corpus.md
+++ docs/corpus.md
@@ -23,3 +23,50 @@ Data Sample:
 >>> y[0] 
 ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-LOC', 'I-LOC']
 ```
+
+## SMP2018 ECDT Human-Computer Dialogue Classification Corpus
+
+https://worksheets.codalab.org/worksheets/0x27203f932f8341b79841d50ce0fd684f/
+
+This dataset is released by the Evaluation of Chinese Human-Computer Dialogue Technology (SMP2018-ECDT)
+task 1 and is provided by the iFLYTEK Corporation, which is a Chinese human-computer dialogue dataset.
+
+```
+      label           query
+0   weather        今天东莞天气如何
+1       map  从观音桥到重庆市图书馆怎么走
+2  cookbook          鸭蛋怎么腌？
+3    health         怎么治疗牛皮癣
+4      chat             唠什么
+```
+
+Usage:
+
+```python
+from kashgari.corpus import SMP2018ECDTCorpus
+
+train_x, train_y = SMP2018ECDTCorpus.load_data('train')
+test_x, test_y = SMP2018ECDTCorpus.load_data('test')
+valid_x, valid_y = SMP2018ECDTCorpus.load_data('valid')
+
+# Change cutter to jieba, need to install jieba first
+train_x, train_y = SMP2018ECDTCorpus.load_data('train', cutter='jieba')
+test_x, test_y = SMP2018ECDTCorpus.load_data('test', cutter='jieba')
+valid_x, valid_y = SMP2018ECDTCorpus.load_data('valid', cutter='jieba')
+```
+
+Data Sample:
+
+```python
+# char cutted
+>>> x[0] 
+[['给', '周', '玉', '发', '短', '信']]
+>>> y[0] 
+['message']
+
+# jieba cutted
+>>> x[0] 
+[['给', '周玉', '发短信']]
+>>> y[0] 
+['message']
+```
--- kashgari/corpus.py
+++ kashgari/corpus.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 import os
+import pandas as pd
 import kashgari.macros as k
 from typing import Tuple, List
 from tensorflow.python.keras.utils import get_file
-
+from kashgari import utils
 
 CORPUS_PATH = os.path.join(k.DATA_PATH, 'corpus')
 
@@ -18,19 +19,24 @@ class ChineseDailyNerCorpus(object):
     __zip_file__name = 'http://storage.eliyar.biz/corpus/china-people-daily-ner-corpus.tar.gz'
 
     @classmethod
-    def load_data(cls, subset_name: str = 'train') -> Tuple[List[List[str]], List[List[str]]]:
+    def load_data(cls,
+                  subset_name: str = 'train',
+                  shuffle: bool = True) -> Tuple[List[List[str]], List[List[str]]]:
         """
         Load dataset as sequence labeling format, char level tokenized
 
-        features: [['海', '钓', '比', '赛', '地', '点', '在', '厦', '门', ...], ...]
+        features: ``[['海', '钓', '比', '赛', '地', '点', '在', '厦', '门', ...], ...]``
 
-        labels: [['O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-LOC', 'I-LOC', ...], ...]
+        labels: ``[['O', 'O', 'O', 'O', 'O', 'O', 'O', 'B-LOC', 'I-LOC', ...], ...]``
 
         Sample::
 
-            x, y = ChineseDailyNerCorpus.load_data('train')
+            train_x, train_y = ChineseDailyNerCorpus.load_data('train')
+            test_x, test_y = ChineseDailyNerCorpus.load_data('test')
+
         Args:
             subset_name: {train, test, valid}
+            shuffle: should shuffle or not, default True.
 
         Returns:
             dataset_features and dataset labels
@@ -47,25 +53,96 @@ class ChineseDailyNerCorpus(object):
         else:
             file_path = os.path.join(corpus_path, 'example.dev')
 
-        data_x, data_y = [], []
+        x_data, y_data = [], []
         with open(file_path, 'r', encoding='utf-8') as f:
             lines = f.read().splitlines()
             x, y = [], []
             for line in lines:
                 rows = line.split(' ')
                 if len(rows) == 1:
-                    data_x.append(x)
-                    data_y.append(y)
+                    x_data.append(x)
+                    y_data.append(y)
                     x = []
                     y = []
                 else:
                     x.append(rows[0])
                     y.append(rows[1])
-        return data_x, data_y
+        if shuffle:
+            x_data, y_data = utils.unison_shuffled_copies(x_data, y_data)
+        return x_data, y_data
+
+
+class SMP2018ECDTCorpus(object):
+    """
+    https://worksheets.codalab.org/worksheets/0x27203f932f8341b79841d50ce0fd684f/
+
+    This dataset is released by the Evaluation of Chinese Human-Computer Dialogue Technology (SMP2018-ECDT)
+    task 1 and is provided by the iFLYTEK Corporation, which is a Chinese human-computer dialogue dataset.
+    sample::
+
+              label           query
+        0   weather        今天东莞天气如何
+        1       map  从观音桥到重庆市图书馆怎么走
+        2  cookbook          鸭蛋怎么腌？
+        3    health         怎么治疗牛皮癣
+        4      chat             唠什么
+    """
+
+    __corpus_name__ = 'SMP2018ECDTCorpus'
+    __zip_file__name = 'http://storage.eliyar.biz/corpus/SMP2018ECDTCorpus.tar.gz'
+
+    @classmethod
+    def load_data(cls,
+                  subset_name: str = 'train',
+                  shuffle: bool = True,
+                  cutter: str = 'char') -> Tuple[List[List[str]], List[List[str]]]:
+        """
+        Load dataset as sequence classification format, char level tokenized
+
+        features: ``[['听', '新', '闻', '。'], ['电', '视', '台', '在', '播', '什', '么'], ...]``
+
+        labels: ``['news', 'epg', ...]``
+
+        Args:
+            subset_name: {train, test, valid}
+            shuffle: should shuffle or not, default True.
+            cutter: sentence cutter, {char, jieba}
+
+        Samples::
+            train_x, train_y = SMP2018ECDTCorpus.load_data('train')
+            test_x, test_y = SMP2018ECDTCorpus.load_data('test')
+
+        Returns:
+            dataset_features and dataset labels
+        """
+
+        corpus_path = get_file(cls.__corpus_name__,
+                               cls.__zip_file__name,
+                               cache_dir=k.DATA_PATH,
+                               untar=True)
+
+        if cutter not in ['char', 'jieba', 'none']:
+            raise ValueError('cutter error, please use one onf the {char, jeiba}')
+
+        df_path = os.path.join(corpus_path, f'{subset_name}.csv')
+        df = pd.read_csv(df_path)
+        if cutter == 'jieba':
+            try:
+                import jieba
+            except ModuleNotFoundError:
+                raise ModuleNotFoundError("please install jieba, `$ pip install jieba`")
+            x_data = [list(jieba.cut(item)) for item in df['query'].to_list()]
+        elif 'char':
+            x_data = [list(item) for item in df['query'].to_list()]
+        y_data = df['label'].to_list()
+
+        if shuffle:
+            x_data, y_data = utils.unison_shuffled_copies(x_data, y_data)
+        return x_data, y_data
 
 
 if __name__ == "__main__":
-    x, y = ChineseDailyNerCorpus.load_data('train')
-    print(x[:1])
-    print(y[:1])
+    x, y = SMP2018ECDTCorpus.load_data('train', cutter='char')
+    print(x[:2])
+    print(y[:2])
     print("Hello world")
