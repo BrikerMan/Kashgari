e17146ef8daf5fddd15dc621cbbb02c92c589963
BrikerMan <eliyar917@gmail.com>
2019-05-22 18:55:10 +0800

    :sparkles: Add two experimental model.

--- kashgari/tasks/labeling/models.py
+++ kashgari/tasks/labeling/models.py
@@ -15,6 +15,8 @@ from tensorflow import keras
 from kashgari.tasks.labeling.base_model import BaseLabelingModel
 from kashgari.layers import L
 
+from keras_self_attention import SeqSelfAttention
+
 
 class BLSTMModel(BaseLabelingModel):
     """Bidirectional LSTM Sequence Labeling Model"""
@@ -59,6 +61,115 @@ class BLSTMModel(BaseLabelingModel):
                                                    name='layer_time_distributed')
 
         tensor = layer_blstm(embed_model.output)
+        tensor = layer_dropout(tensor)
+        output_tensor = layer_time_distributed(tensor)
+
+        self.tf_model = keras.Model(embed_model.inputs, output_tensor)
+
+
+class BLSTMAttentionModel(BaseLabelingModel):
+    """Bidirectional LSTM Self Attention Sequence Labeling Model"""
+    __architect_name__ = 'BLSTMAttentionModel'
+
+    @classmethod
+    def get_default_hyper_parameters(cls) -> Dict[str, Dict[str, Any]]:
+        """
+        Get hyper parameters of model
+        Returns:
+            hyper parameters dict
+        """
+        return {
+            'layer_blstm': {
+                'units': 128,
+                'return_sequences': True
+            },
+            'layer_self_attention': {
+                'attention_activation': 'sigmoid'
+            },
+            'layer_dropout': {
+                'rate': 0.4
+            },
+            'layer_time_distributed': {
+                'activation': 'softmax'
+            }
+        }
+
+    def build_model_arc(self):
+        """
+        build model architectural
+        """
+        output_dim = len(self.pre_processor.label2idx)
+        config = self.hyper_parameters
+        embed_model = self.embedding.embed_model
+
+        layer_blstm = L.Bidirectional(L.LSTM(**config['layer_blstm']),
+                                      name='layer_blstm')
+        layer_self_attention = SeqSelfAttention(**config['layer_self_attention'],
+                                                name='layer_self_attention')
+        layer_dropout = L.Dropout(**config['layer_dropout'],
+                                  name='layer_dropout')
+
+        layer_time_distributed = L.TimeDistributed(L.Dense(output_dim,
+                                                           **config['layer_time_distributed']),
+                                                   name='layer_time_distributed')
+
+        tensor = layer_blstm(embed_model.output)
+        tensor = layer_self_attention(tensor)
+        tensor = layer_dropout(tensor)
+        output_tensor = layer_time_distributed(tensor)
+
+        self.tf_model = keras.Model(embed_model.inputs, output_tensor)
+
+
+class SeqSelfAttentionModel(BaseLabelingModel):
+    """Bidirectional LSTM Self Attention Sequence Labeling Model"""
+    __architect_name__ = 'SeqSelfAttentionModel'
+
+    @classmethod
+    def get_default_hyper_parameters(cls) -> Dict[str, Dict[str, Any]]:
+        """
+        Get hyper parameters of model
+        Returns:
+            hyper parameters dict
+        """
+        return {
+            'layer_self_attention_0': {
+                'units': 32,
+                'attention_activation': 'relu'
+            },
+            'layer_self_attention_1': {
+                'units': 32,
+                'attention_activation': 'sigmoid'
+            },
+            'layer_dropout': {
+                'rate': 0.4
+            },
+            'layer_time_distributed': {
+                'activation': 'softmax'
+            }
+        }
+
+    def build_model_arc(self):
+        """
+        build model architectural
+        """
+        output_dim = len(self.pre_processor.label2idx)
+        config = self.hyper_parameters
+        embed_model = self.embedding.embed_model
+
+        layer_self_attention_0 = SeqSelfAttention(**config['layer_self_attention_0'],
+                                                  name='layer_self_attention0')
+        layer_self_attention_1 = SeqSelfAttention(**config['layer_self_attention_1'],
+                                                  name='layer_self_attention1')
+        layer_dropout = L.Dropout(**config['layer_dropout'],
+                                  name='layer_dropout')
+
+        layer_time_distributed = L.TimeDistributed(L.Dense(output_dim,
+                                                           **config['layer_time_distributed']),
+                                                   name='layer_time_distributed')
+
+        tensor = layer_self_attention_0(embed_model.output)
+        tensor = layer_self_attention_1(tensor)
         tensor = layer_dropout(tensor)
         output_tensor = layer_time_distributed(tensor)
 
@@ -131,35 +242,10 @@ if __name__ == "__main__":
     x, y = ChineseDailyNerCorpus.load_data()
 
     # # Multi Input model
-    old_fashion_model = CNNLSTMModel()
+    old_fashion_model = SeqSelfAttentionModel()
 
     import numpy as np
 
-
-    def demo_acc(y_true, y_pred):
-        print(np.array(y_true))
-        print(np.array(y_true))
-        return np.random.rand(len(y_pred))
-
-
     old_fashion_model.build_model(x, y)
-    old_fashion_model.compile_model(metrics=['acc', demo_acc])
+    old_fashion_model.compile_model(metrics=['acc'])
     old_fashion_model.fit(x, y, epochs=1)
-    #
-    # # Old fashion model
-    # old_fashion_model = CNNLSTMModel()
-    # old_fashion_model.fit(x, y, epochs=1)
-
-    # Model For pros 1
-    # w2v_path = os.path.join(kashgari.utils.get_project_path(), 'tests/test-data/sample_w2v.txt')
-    #
-    # embedding = WordEmbedding(task=kashgari.LABELING,
-    #                           w2v_path=w2v_path,
-    #                           sequence_length='variable')
-    # labeling1 = BLSTMModel(embedding=embedding)
-    # labeling1.fit((x, x), y)
-    #
-    # hyper_parameters = BLSTMModel.get_default_hyper_parameters()
-    # hyper_parameters['layer_blstm']['units'] = 12
-    # labeling_model = BLSTMModel(hyper_parameters=hyper_parameters)
-    # labeling_model.fit(x, y)
