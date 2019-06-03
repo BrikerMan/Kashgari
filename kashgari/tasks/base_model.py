4c5d3746d4295ee0b907a58a7e02dbccade69541
hahahu <im@huhaoyu.com>
2019-06-03 18:47:22 +0800

    features: export saved model for serving

--- kashgari/tasks/base_model.py
+++ kashgari/tasks/base_model.py
@@ -11,11 +11,12 @@
 from typing import Dict, Any, List, Optional, Union, Tuple
 
 import os
+import time
 import json
 import pathlib
 import logging
 import numpy as np
-from tensorflow import keras
+from tensorflow import keras, saved_model
 from kashgari import utils
 from kashgari.embeddings import BareEmbedding
 from kashgari.embeddings.base_embedding import Embedding
@@ -286,6 +287,22 @@ class BaseModel(object):
 
         self.tf_model.save_weights(os.path.join(model_path, 'model.h5'))
         logging.info('model saved to {}'.format(os.path.abspath(model_path)))
+
+    def export(self, export_path: str, inputs: Optional[Dict] = None, outputs: Optional[Dict] = None):
+        pathlib.Path(export_path).mkdir(exist_ok=True, parents=True)
+
+        ts = round(time.time())
+        export_path = os.path.join(export_path, str(ts))
+
+        if inputs is None:
+            inputs = {i.name: i for i in self.tf_model.inputs}
+        if outputs is None:
+            outputs = {o.name: o for o in self.tf_model.outputs}
+        sess = keras.backend.get_session()
+        saved_model.simple_save(session=sess,
+                                export_dir=export_path,
+                                inputs=inputs,
+                                outputs=outputs)
 
 
 if __name__ == "__main__":
