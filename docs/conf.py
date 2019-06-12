1baead99f906f3bab125c93dece07e5c74bd121f
BrikerMan <eliyar917@gmail.com>
2019-06-12 13:25:09 +0800

    :memo: Writing docs.

--- docs/conf.py
+++ docs/conf.py
@@ -24,23 +24,24 @@ project = 'Kashgari'
 copyright = '2019, BrikerMan'
 author = 'BrikerMan'
 
-
 # -- General configuration ---------------------------------------------------
 source_suffix = ['.rst', '.md']
 
-# source_parsers = {
-#     '.md': CommonMarkParser
-# }
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.doctest',
     'recommonmark',
-    'sphinx.ext.autosummary',
-    'sphinx_markdown_tables'
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,6 +51,12 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+# Autodoc settings
+autodoc_default_flags = ['members', 'undoc-members', 'inherited-members']
+
+# Autosummary settings
+autosummary_generate = True
 
 autoclass_content = "both"
 autodoc_member_order = 'bysource'
@@ -68,23 +75,39 @@ napoleon_use_ivar = True
 napoleon_use_param = True
 napoleon_use_rtype = True
 
+# https://help.farbox.com/pygments.html
+pygments_style = 'tango'
 
 # -- Options for HTML output ---------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    # 'canonical_url': '',
+    # 'analytics_id': '',
+    # 'logo_only': False,
+    'display_version': False,
+    # 'prev_next_buttons_location': 'bottom',
+    'prev_next_buttons_location': None,
+    'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 
 import kashgari
@@ -96,8 +119,15 @@ release = version
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
-        # 'url_resolver': lambda url: github_doc_root + url,
+        #'url_resolver': lambda url: github_doc_root + url,
         'auto_toc_tree_section': 'Contents',
         'enable_eval_rst': True,
     }, True)
     app.add_transform(AutoStructify)
+    app.add_stylesheet('theme.css')
+
+
+intersphinx_mapping = {
+    'python': ('http://docs.python.org/', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+}
