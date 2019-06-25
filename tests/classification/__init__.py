c6c492fa72f6abfdad37ccf8931d0d587f386f27
BrikerMan <eliyar917@gmail.com>
2019-06-25 10:34:34 +0800

    :green_heart: Fixing CI Build.

---
+++ tests/classification/__init__.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+# author: BrikerMan
+# contact: eliyar917@gmail.com
+# blog: https://eliyar.biz
+
+# file: __init__.py
+# time: 10:34
+
+
+if __name__ == "__main__":
+    print("hello, world")
