506d102c6f9e8139a783b63acbed65ecd987e6e2
Travis CI User <travis@example.org>
2019-12-27 08:01:14 +0000

    Deployed 9d72b7b with MkDocs version: 1.0.4

---
+++ static/js/baidu-static.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5bbafbb1b5e47d6c8da68e6875345893";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
